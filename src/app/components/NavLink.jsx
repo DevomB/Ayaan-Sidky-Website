df9b50e191af6637a75e983b8c9658c0113d9595
Devom Brahmbhatt <Devom.b@yahoo.com>
2024-07-23 12:39:39 -0700

    Added Scroll Animation

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,11 +1,21 @@
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
 const NavLink = ({ href, title }) => {
+  const handleClick = (event) => {
+    event.preventDefault();
+    const targetId = href.substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
-    <Link
-      href={href}
-      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
-    >
+    <Link href={href} onClick={handleClick} className="scroll-link">
       {title}
     </Link>
   );
