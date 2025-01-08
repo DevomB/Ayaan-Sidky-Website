13744c4dd8b24e762fa055fc94b90d5e8ca9b16a
Devom Brahmbhatt <70069338+DevomB@users.noreply.github.com>
2025-01-08 10:57:29 -0800

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import Image from 'next/image';
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between items-center">
-        <div className="logo-container">
-          <Image src="/images/new-as-trans-logo.png" alt="Logo" layout="intrinsic" width={75} height={75} />
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white bg-black">
+      <div className="container p-12 flex flex-col items-center">
+        <div className="logo-container mb-4">
+          <Image
+            src="/images/new-as-trans-logo.png"
+            alt="Logo"
+            layout="intrinsic"
+            width={75}
+            height={75}
+          />
         </div>
-        <p className="text-slate-600">All rights reserved.</p>
+        <a
+          href="https://github.com/devom-brahmbhatt"
+          className="text-lg text-[#FFD700] font-bold hover:underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Made by Devom Brahmbhatt
+        </a>
       </div>
     </footer>
   );
